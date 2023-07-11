7f6523f1177a92fb1228cf39810a9588d527e724
wagnerpx <impussivo@gmail.com>
2023-07-11 00:51:05 -0300

    Atualização

--- src/js/index.js
+++ src/js/index.js
@@ -29,11 +29,36 @@ const personagens = document.querySelectorAll('.personagem');
 personagens.forEach((personagem) =>{
     personagem.addEventListener('mouseenter', () => {
 
+        if(window.innerWidth < 450) {
+            window.screenTop({top: 0, behavior: 'smooth' });
+        }
+
         // passo 3 - verificar se já exista um personagem selecionado, se sim, devemos remover a seleção dele
         const personagemSelecionado = document.querySelector('.selecionado');
         personagemSelecionado.classList.remove('selecionado');
         personagem.classList.add('selecionado');
 
+        //OBJETIVO 2 - quando passar o mouse em cima do personagem na listagem, trocar a imagem, o nome e a descrição do personagem grande
+
+        // passo 1 - pegar o elemento do personagem grande pra adicionar as informações nele
+
+        const imagempersonagemGrande = document.querySelector('.personagem-grande');
+
+        //passo 2 - alterar a imagem do personagem grande
+
+        const idpersonagem = personagem.attributes.id.value;
+        imagempersonagemGrande.src = `./src/imagens/card-${idpersonagem}.png`
+
+        // passo 3 - alterar o nome do personagem grande
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        // passo 4 - alterar a descrição do personagem grande
+
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
 
     })
 } )
